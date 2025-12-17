a4d2114f86b0c2a74674d59af3c2a4b6c37c5b07
Sasi Kumar <sasikumarnelavala@gmail.com>
2025-12-16 23:10:17 -0700

    Countvowels

--- java day6/Stringdemo2.java
+++ java day6/Stringdemo2.java
@@ -1,4 +1,4 @@
-public class Stringdemo2 {
+  public class Stringdemo2 {
     public static void main(String[] args) {
         // StringBuffer example
         StringBuffer sb = new StringBuffer("Hello");
